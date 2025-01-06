d0529e115b879aca509b016a3ea4217b158b28d8
xexubonete <xexubonete@gmail.com>
2025-01-06 23:30:14 +0100

    Changing output of N/A to 0KB/s

--- items/widgets/disk_stats.lua
+++ items/widgets/disk_stats.lua
@@ -1,7 +1,5 @@
--- Update with path to stats_provider
 sbar.exec('killall stats_provider >/dev/null; $CONFIG_DIR/sketchybar-system-stats/target/release/stats_provider --cpu usage --disk usage --memory usage')
 
--- Add 'disk_usage' item to the bar
 local disk_usage = sbar.add('item', 'disk_usage', {
     position = 'center',
     padding_right = 4,
@@ -14,36 +12,30 @@ local disk_usage = sbar.add('item', 'disk_usage', {
     }
 })
 
--- Subscribe to system stats updates
 disk_usage:subscribe('system_stats', function(env)
-    -- Capture system stats from the environment
     local network_rx
     local network_tx
     if env.NETWORK_RX_en0 == "0KB/s" and env.NETWORK_TX_en0 == "0KB/s" then
-        network_rx = env.NETWORK_RX_en6 or "N/A"
-        network_tx = env.NETWORK_TX_en6 or "N/A"
+        network_rx = env.NETWORK_RX_en6 or "0KB/s"
+        network_tx = env.NETWORK_TX_en6 or "0KB/s"
     else
-        network_rx = env.NETWORK_RX_en0 or "N/A"
-        network_tx = env.NETWORK_TX_en0 or "N/A"
+        network_rx = env.NETWORK_RX_en0 or "0KB/s"
+        network_tx = env.NETWORK_TX_en0 or "0KB/s"
     end
     
     local cpu_temp = env.CPU_TEMP or "N/A"
     local ram_used = env.RAM_USED or "N/A"
 
-    -- Process CPU_USAGE
-    local cpu_usage = env.CPU_USAGE or "N/A" -- Initial value
-
-    -- Build the label to display
+    local cpu_usage = env.CPU_USAGE or "N/A"
+    
     local ram_used_with_max = string.format(
     "%s - %s - %s - %s - %s",
     cpu_usage, cpu_temp, ram_used, network_rx, network_tx
     )
 
-    -- Update the bar item
     disk_usage:set { label = ram_used_with_max }
     end)
 
-    -- Execute the external stats provider and configure SketchyBar
     os.execute([[
     export CONFIG_DIR=~/.config
     ## Start the helper with desired CLI args
